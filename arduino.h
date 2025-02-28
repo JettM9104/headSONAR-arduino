4eadb88a19558ecfea45f17d9e257a5e4ab0a6e4
JMu <64600882+JettM9104@users.noreply.github.com>
2025-02-28 11:59:17 -0500

    Added button functionality

--- arduino.h
+++ arduino.h
@@ -10,15 +10,20 @@ public:
   void begin(int chanel) { channel = chanel; }
   void print(std::string hi) {}
   void println(std::string hi) {}
+  void println(int skibidi) {}
   void println(float skibidi) {}
-
+  void print(int skibidi) {}
+  void print(float skibidi) {}
 };
 
 serial Serial;
 
 void pinMode(int number, mode io) {}
 void digitalWrite(int number, ud state) {}
-float pulseIn(int number, ud state) {}
+float pulseIn(int number, ud state) { return 0.0; }
 void delayMicroseconds(int time) {}
 void delay(int time) {}
+void tone(int port, int pitch) {}
+void noTone(int port) {}
+bool digitalRead(int port) { return 0; }
 
